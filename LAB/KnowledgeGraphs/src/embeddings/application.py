e7d19570fe98c21c75e7f3171868d2e0e99e0161
eZWALT <waltertheshadow333@gmail.com>
2025-06-11 17:11:41 +0200

    Intial Project Structure

---
+++ LAB/KnowledgeGraphs/src/embeddings/application.py
@@ -0,0 +1,29 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+# ===----------------------------------------------------------------------===#
+# Embeddings Applications                                                     #
+#                                                                             #
+# Section C.4                                                                 #                                                                          
+# Author: Walter J.T.V                                                        #
+# ===----------------------------------------------------------------------===#
+
+# Set up the GraphDB endpoint
+sparql = SPARQLWrapper("http://localhost:7200/repositories/SDM2")
+
+    
+    
+if __name__ == "__main__":
+    sparql.setQuery("""
+        SELECT ?s ?p ?o
+        WHERE {
+            ?s ?p ?o .
+        }
+        LIMIT 100
+    """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    # Extract and print
+    for result in results["results"]["bindings"]:
+        print(result["s"]["value"], result["p"]["value"], result["o"]["value"])
