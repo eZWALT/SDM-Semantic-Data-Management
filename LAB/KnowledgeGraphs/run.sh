c023c80b823b7093b7207373f54bde4877224b29
eZWALT <waltertheshadow333@gmail.com>
2025-06-15 23:12:02 +0200

    pushete final

--- LAB/KnowledgeGraphs/run.sh
+++
@@ -1,7 +0,0 @@
-docker pull ontotext/graphdb:11.0.1
-docker run -d -p 127.0.0.1:7200:7200 -v $(pwd)/data:/opt/graphdb/home --name graphdb -t ontotext/graphdb:11.0.1
-docker exec -it graphdb bash
-
-
-# docker stop graphdb
-#docker rm graphdb
